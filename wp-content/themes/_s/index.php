768123e6fb0e5d9e49e8fafb842eb0fdd37453da
Bret Cameron <bretcameron@gmail.com>
2019-01-16 23:37:56 +0000

    Display lists within articles in the same font as <p> tags

--- wp-content/themes/_s/index.php
+++ wp-content/themes/_s/index.php
@@ -29,7 +29,60 @@ $myFeaturedPost->query('posts_per_page=1');
 while ($myFeaturedPost->have_posts()) :
 	$myFeaturedPost->the_post();
 $content = apply_filters('the_content', $post->post_content);
-require 'post-preview.php';
+?>
+
+<!-- IMAGE -->
+
+			<div id="featured-post" class="latest-post">
+				<div class="image-wrapper">
+					<div class="inner-image">
+						<?php marketeer_now_post_thumbnail(); ?>
+					</div>
+				</div>
+		<!-- CATEGORY -->
+				<div class="flex-category">
+					<div class="box-left">
+						<!-- Transparent Placeholder -->
+					</div>
+					<div class="box-right">
+						<?php the_category() ?>
+					</div>
+				</div>
+		<!-- TITLE -->
+				<a class="permalink" href="<?php the_permalink(); ?>">
+					<h1>
+						<?= the_title() ?>
+					</h1>
+                </a>
+		<!-- CONTENT PREVIEW -->
+				<a class="permalink" href="<?php the_permalink(); ?>">
+					<p class="content-preview">
+						<?php
+					if (has_excerpt()) {
+						$excerpt = wp_strip_all_tags(get_the_excerpt());
+						if (preg_match("/[\.!?,;:]$/", $excerpt)) {
+							echo $excerpt;
+						} else {
+							echo $excerpt . '...';
+						};
+					} else {
+						echo contentPreview($content) . '...';
+					};
+					?>
+                    </p>
+                    <br>
+				</a>                
+		<!-- READ TIME -->
+				<a class="permalink" href="<?php the_permalink(); ?>">
+					<div class="read-time">
+						<?= getReadingTime($content) ?>
+					</div>
+                </a>
+			</div>
+
+
+<?php
+// require 'post-preview.php';
 endwhile;
 wp_reset_postdata();
 ?>
