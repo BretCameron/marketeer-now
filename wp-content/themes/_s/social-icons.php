1cb34c7d9f6fa60d663228012cc63ee4ec9b1553
Bret Cameron <bretcameron@gmail.com>
2019-01-17 18:42:42 +0000

    Add social-icons.php

---
+++ wp-content/themes/_s/social-icons.php
@@ -0,0 +1,5 @@
+<a href="https://www.facebook.com/sharer/sharer.php?u=<?= $currentURL ?>" target="_blank"><img class="social-icon social-icon-large" src="https://dl.dropboxusercontent.com/s/abnb2wuf4tjrh6u/Facebook.svg?dl=0" alt="Share on Facebook"></a>
+<a href="https://twitter.com/share?ref_src=<?= $currentURL ?>" target="_blank"><img class="social-icon social-icon-large" src="https://dl.dropboxusercontent.com/s/ob40ouepc8qfe76/Twitter.svg?dl=0" alt="Tweet"></a>
+<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?= $currentURL ?>&title=LinkedIn%20Developer%20Network
+&summary=My%20favorite%20developer%20program&source=LinkedIn" target="_blank"><img class="social-icon social-icon-large" src="https://dl.dropboxusercontent.com/s/2amnqal3ghrvfrq/LinkedIn.svg?dl=0" alt="Share on LinkedIn"></a>
+<a href="mailto:?subject=<?= the_title() ?>&body=Article: <?= $currentURL ?>"><img class="social-icon social-icon-large" src="https://dl.dropboxusercontent.com/s/gooz572gd6ogwzs/Email.svg?dl=0" alt="Email"></a>
