ae40f2d10d905f5a1ab5d9575b724351a264f74b
Bret Cameron <bretcameron@gmail.com>
2019-01-19 12:11:50 +0000

    Fix vertical icon links (too wide) and add category pages

--- wp-content/themes/_s/archive.php
+++ wp-content/themes/_s/archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -12,42 +13,83 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container">
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
+			// get_the_archive_title('<h2 class="page-title">', '</h2>');
+			$archiveTitle = get_the_archive_title();
+			$archiveTitle = str_replace('Category: ', '', $archiveTitle);
+			?>
+			<br>
+			<h2><?php echo $archiveTitle; ?></h2>
+
+			<div class="author-page-flex">
+		<?php if (have_posts()) : ?>
+			<header class="page-header">
+
+			<?php
+		the_archive_description('<div class="archive-description">', '</div>');
+		?>
 			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		while (have_posts()) :
+			the_post();
 
 				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+		 * Include the Post-Type-specific template for the content.
+		 * If you want to override this in a child theme, then include a file
+		 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+		 */
+		// $content = apply_filters('the_content', $post->post_content);
+		$content = apply_filters('the_content', $post->post_content);
+		// echo $content;
+		get_template_part('template-parts/content', get_post_type());
 
-			endwhile;
+		endwhile;
 
-			the_posts_navigation();
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part('template-parts/content', 'none');
 
 		endif;
 		?>
 
+				</div><!-- .author-page-flex -->
+			</div><!-- .container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
+
+?>
+
+<!-- FUNCTIONS -->
+
+<?php
+// FUNCTION TO GET READING TIME
+function getReadingTime($content, $wordsPerMinute = 200, $message = 'minute read')
+{
+	$readingTime = round(str_word_count(strip_tags($content)) / $wordsPerMinute);
+	if ($readingTime == 0) {
+		$readingTime += 1;
+	}
+	return $readingTime . ' ' . $message;
+};
+
+
+// FUNCTION TO RETURN A PREVIEW OF THE ARTICLE CONTENT
+function contentPreview($content, $numOfWords = 10)
+{
+	$content = str_replace(array("\r", "\n"), '', $content);
+	$spaceString = str_replace('<', ' <', $content);
+	$doubleSpace = strip_tags($spaceString);
+	$singleSpace = str_replace('  ', ' ', $doubleSpace);
+	$pieces = explode(" ", $singleSpace);
+	$first_part = implode(" ", array_splice($pieces, 0, $numOfWords));
+	return $first_part;
+};
--- wp-content/themes/_s/template-parts/content.php
+++ wp-content/themes/_s/template-parts/content.php
@@ -10,57 +10,57 @@
 
 ?>
 
-	<?php marketeer_now_post_thumbnail(); ?>
 
+<!-- IMAGE -->
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-	if (is_singular()) :
-		the_title('<h1 class="entry-title">', '</h1>');
-	else :
-		the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-	endif;
+			<div class="latest-post">
+				<div class="image-wrapper">
+					<div class="inner-image">
+						<?php marketeer_now_post_thumbnail(); ?>
+					</div>
+				</div>
+		<!-- CATEGORY -->
+				<div class="flex-category">
+					<div class="box-left">
+						<!-- Transparent Placeholder -->
+					</div>
+					<div class="box-right">
+						<?php the_category() ?>
+					</div>
+				</div>
+		<!-- TITLE -->
+				<a class="permalink" href="<?php the_permalink(); ?>">
+					<h1>
+						<?= the_title() ?>
+					</h1>
+                </a>
+		<!-- CONTENT PREVIEW -->
+				<a class="permalink" href="<?php the_permalink(); ?>">
+					<p class="content-preview">
+					<?php
+				$content = apply_filters('the_content', $post->post_content);
+				?>
+						<?php
+					if (has_excerpt()) {
+						$excerpt = wp_strip_all_tags(get_the_excerpt());
+						if (preg_match("/[\.!?,;:]$/", $excerpt)) {
+							echo $excerpt;
+						} else {
+							echo $excerpt . '...';
+						};
+					} else {
+						echo contentPreview($content) . '...';
+					};
+					?>
+                    </p>
+                    <br>
+				</a>                
+		<!-- READ TIME -->
+				<a class="permalink" href="<?php the_permalink(); ?>">
+					<div class="read-time">
+						<?php getReadingTime($content) ?>
+					</div>
+                </a>
+			</div>
 
-	if ('post' === get_post_type()) :
-	?>
-	
-			<div class="entry-meta">
-				<?php
-			marketeer_now_posted_on();
-			marketeer_now_posted_by();
-			?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-
-	<div class="entry-content">
-		<?php
-	the_content(sprintf(
-		wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-			__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'marketeer-now'),
-			array(
-				'span' => array(
-					'class' => array(),
-				),
-			)
-		),
-		get_the_title()
-	));
-
-	wp_link_pages(array(
-		'before' => '<div class="page-links">' . esc_html__('Pages:', 'marketeer-now'),
-		'after' => '</div>',
-	));
-	?>
-
-
-</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php marketeer_now_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
 
