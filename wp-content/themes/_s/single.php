5fb0be33dc07c4b6dca5663c942dd662871974d3
Bret Cameron <bretcameron@gmail.com>
2019-01-19 19:58:04 +0000

    Expand image button on articles, fix vertical icon spacing with JQuery

--- wp-content/themes/_s/single.php
+++ wp-content/themes/_s/single.php
@@ -56,9 +56,19 @@ while (have_posts()) :
 </div> -->
 
 
+
 <?php
 marketeer_now_post_thumbnail();
 ?>
+
+<svg id="expand-image" viewBox="0 0 100 100">
+	<title>Expand Image</title>
+	<path d="M86.86,37.22c-.58-.53-1.18-1-1.73-1.59C82.4,32.8,79.66,30,77,27.09c-.77-.82-1.25-.74-2,0Q66.29,36.3,57.52,45.42c-.44.45-.91.88-1.34,1.31l-4.36-4.64c.74-.8,1.56-1.71,2.41-2.59L70.37,22.65c1.17-1.21,1.16-1.22,0-2.43q-4.5-4.71-9-9.43a6.46,6.46,0,0,1-.51-.74h26Z"/>
+	<path d="M11.72,61.36c.6.56,1.23,1.09,1.8,1.68,2.76,2.86,5.52,5.72,8.24,8.61.64.68,1.06.68,1.71,0q9-9.48,18.11-18.9c.28-.3.58-.58.87-.86l4.33,4.61c-.15.19-.38.51-.65.8L28.47,75.73a6,6,0,0,1-.54.58c-.75.63-.59,1.17,0,1.81,3.05,3.15,6.08,6.34,9.1,9.51a8.4,8.4,0,0,1,.67.9h-26Z"/>
+	<path d="M96.1,100H4.15a3.91,3.91,0,0,1-3.9-3.9V4.15A3.91,3.91,0,0,1,4.15.25H96.1a3.91,3.91,0,0,1,3.9,3.9V96.1A3.91,3.91,0,0,1,96.1,100ZM4.15,3.16a1,1,0,0,0-1,1V96.1a1,1,0,0,0,1,1H96.1a1,1,0,0,0,1-1V4.15a1,1,0,0,0-1-1Z"/>
+</svg>
+
+
 
 <div class="vertical-icons">
 	<?php require('social-icons.php'); ?>
@@ -289,16 +299,63 @@ $(window).scroll(function (){
 $('.article-block > .post-thumbnail > img').click(function () {
 	if ($('.lightbox-window').hasClass('hidden')) {
 		$('.lightbox-window').removeClass('hidden');
+		$('#expand-image').addClass('hidden');
 	};
 });
 
 $('.lightbox-window').click(function () {
 	if (!$('.lightbox-window').hasClass('hidden')) {
 		$('.lightbox-window').addClass('hidden');
+		$('#expand-image').removeClass('hidden');
 	};
 });//.children().click(function(e) {
 //   return false;
 // });;
+
+
+// EXPAND IMAGE ICON
+
+$(document).ready(positionExpandImage);
+$(window).resize(positionExpandImage);
+
+function positionExpandImage() {
+// SET DESIRED STANDARDS (pixels)
+	const normalSize = 30;
+	const normalPadding = 10;
+
+	// RESIZE
+	let initialHeight = $('.article-block > .post-thumbnail > img').height();
+	let relativeHeight = initialHeight / 371.188;
+	$('#expand-image').css('width', normalSize * Math.sqrt(relativeHeight)).css('height', normalSize * Math.sqrt(relativeHeight));
+	
+	// DEFINE COORDS
+	let imageTop = $('.article-block > .post-thumbnail > img').offset().top;
+	let imageLeft = $('.article-block > .post-thumbnail > img').offset().left;
+	let imageWidth = $('.article-block > .post-thumbnail > img').width();
+	let imageHeight = $('.article-block > .post-thumbnail > img').height();
+	let iconWidth = $('#expand-image').width();
+	let iconHeight = $('#expand-image').height();
+
+// REPOSITION
+	$('#expand-image').css('top',imageTop + normalPadding * Math.sqrt(relativeHeight)).css('left',imageLeft + imageWidth - iconWidth - normalPadding * Math.sqrt(relativeHeight));
+	
+};
+
+// REDUCE VERTICAL GAP CREATED BY VERTICAL SOCIAL ICONS
+
+let verticalIconsHeight = $('.vertical-icons').height();
+
+$(document).ready(removeVerticalIconGap);
+$(window).resize(removeVerticalIconGap);
+
+function removeVerticalIconGap() {
+	if ($('.vertical-icons').css('display') == 'none') {
+		$('.article-block > .post-thumbnail').css('margin-bottom',-0);
+	} else {
+		$('.article-block > .post-thumbnail').css('margin-bottom',-verticalIconsHeight);
+	};
+};
+
 
 </script>
 
