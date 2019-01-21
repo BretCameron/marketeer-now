ba549bde528cba5c8d2fffe8a6a52da34ffeceb7
Bret Cameron <bretcameron@gmail.com>
2019-01-21 10:32:35 +0000

    Add Bing Webmaster tag

--- wp-content/themes/_s/header.php
+++ wp-content/themes/_s/header.php
@@ -9,19 +9,29 @@
 
 ?>
 
-<!-- The header contains the HTML and Head tags. -->
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Google Search Console -->
 	<meta name="google-site-verification" content="GWqCP2-qh8LLG5fnvd95PEkD0K80qZ-os4JVBqR_wNU" />
+	<!-- Bing Webmaster -->
+	<meta name="msvalidate.01" content="EEC1BA5595057A957BA1213EF1E09C34" />
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link href="https://fonts.googleapis.com/css?family=Muli:200,300,400" rel="stylesheet">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113865004-2"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-113865004-2'); </script>
+
 	<?php wp_head(); ?>
 </head>
 
-<!-- The header opens the body tag, which will be closed in the footer.  -->
+<?php //The header opens the body tag, which will be closed in the footer. ?>
 <body <?php body_class(); ?>>
 
     <header id="nav-header">
